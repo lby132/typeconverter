07c9594ef9f69d4da977b29abeb7bf5bffd282e2
lby132 <quddbscjswow@naver.com>
2022-03-01 20:26:03 +0900

    컨버터 적용

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,7 +1,9 @@
 package hello.typeconverter.controller;
 
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +15,19 @@ public class HelloController {
     public String helloV1(HttpServletRequest request) {
         String data = request.getParameter("data");//문자 타입으로 조회
         Integer intValue = Integer.valueOf(data); //숫자 타입으로 변경
+        return "ok";
+    }
+
+    @GetMapping("hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort ip= " + ipPort.getIp());
+        System.out.println("ipPort port= " + ipPort.getPort());
         return "ok";
     }
 }
